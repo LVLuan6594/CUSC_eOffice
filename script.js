ecd76bbd78d3a501d5a40049c5b4fa89cce0fffb
LVLuan6594 <leluan652024@gmail.com>
2025-07-04 07:31:41 +0700

    Update block F12 in .js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Block review code
+document.addEventListener('contextmenu', function (e) {
+  e.preventDefault();
+});
+document.addEventListener('keydown', function (e) {
+  if (
+    e.key === 'F12' ||
+    (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'J' || e.key === 'C')) ||
+    (e.ctrlKey && e.key === 'U')
+  ) {
+    e.preventDefault();
+  }
+});
 // Initialize AOS
 AOS.init({ duration: 800, once: true });
 
